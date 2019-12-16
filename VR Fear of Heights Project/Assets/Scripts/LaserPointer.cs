b8afcd04e5055dd5d2c02d6b784a2c551f4ec95a
Jeff <jefflenane@gmail.com>
2019-12-16 14:10:44 +0100

    stuff for UI

--- VR Fear of Heights Project/Assets/Scripts/LaserPointer.cs
+++ VR Fear of Heights Project/Assets/Scripts/LaserPointer.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using Valve.VR;
 
 public class LaserPointer : MonoBehaviour
@@ -8,6 +10,7 @@ public class LaserPointer : MonoBehaviour
     public SteamVR_Input_Sources handType;
     public SteamVR_Behaviour_Pose controllerPose;
     public SteamVR_Action_Boolean teleportAction;
+    public SteamVR_Action_Boolean selectAction;
     public GameObject laserPrefab; // 1
     private GameObject laser; // 2
     private Transform laserTransform; // 3
@@ -21,6 +24,8 @@ public class LaserPointer : MonoBehaviour
     public Vector3 teleportReticleOffset;
     public LayerMask teleportMask;
     private bool shouldTeleport;
+
+    public Image fadeMask;
 
 
     // Start is called before the first frame update
@@ -50,6 +55,7 @@ public class LaserPointer : MonoBehaviour
                 shouldTeleport = true;
 
             }
+            HandlePointerIn(hit);
         }
         else // 3
         {
@@ -60,6 +66,10 @@ public class LaserPointer : MonoBehaviour
         if (teleportAction.GetStateUp(handType) && shouldTeleport)
         {
             Teleport();
+        }
+        if (selectAction.GetStateDown(handType))
+        {
+            ExecuteUI();
         }
     }
 
@@ -89,5 +99,29 @@ public class LaserPointer : MonoBehaviour
         difference.y = 0;
         // 5
         cameraRigTransform.position = hitPoint + difference;
+    }
+
+    private void HandlePointerIn(RaycastHit hit)
+    {
+        var button = hit.transform.GetComponent<Button>();
+        if (button != null)
+        {
+            button.Select();
+        }
+    }
+    private void ExecuteUI()
+    {
+        if (EventSystem.current.currentSelectedGameObject != null)
+        {
+            ExecuteEvents.Execute(EventSystem.current.currentSelectedGameObject, new PointerEventData(EventSystem.current), ExecuteEvents.submitHandler);
+        }
+    }
+
+    private IEnumerator FadeToBlackAndFadeBack()
+    {
+        fadeMask.color = Color.Lerp(new Color(0, 0, 0, 0), new Color(0, 0, 0, 100), 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        Teleport();
+        fadeMask.color = Color.Lerp(new Color(0, 0, 0, 100), new Color(0, 0, 0, 0), 0.5f);
     }
 }
