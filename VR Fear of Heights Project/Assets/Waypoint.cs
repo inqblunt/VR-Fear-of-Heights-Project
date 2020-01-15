5bf33d8a10785e920ab03b1867cf83cc89ad2792
Jeff <jefflenane@gmail.com>
2020-01-15 09:41:04 +0100

    waypoints and objective

---
+++ VR Fear of Heights Project/Assets/Waypoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public WaypointManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (var i = 0; i < manager.Waypoints.Length; i++)
+        {
+            if (manager.Waypoints[i] == gameObject)
+            {
+                manager.Waypoints[i + 1].gameObject.SetActive(true);
+            }
+        }
+        manager.ScoreCount++;
+        Destroy(gameObject);        
+    }
+}
---
+++ VR Fear of Heights Project/Assets/WaypointManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    public GameObject[] Waypoints;
+    public int ScoreCount;
+    public GameObject WinGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WinGame.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ScoreCount >= 3)
+        {
+            WinGame.SetActive(true);
+        }
+    }
+}
